d868392db24bd210b998f82dd00bb0ab3584a741
Sambou Diao <sam.diao90@gmail.com>
2026-01-05 21:24:33 +0100

    MAJ

--- src/app.py
+++ src/app.py
@@ -9,11 +9,9 @@ from pydantic import BaseModel
 class CycleInput(BaseModel):
     ps2_mean: float
     ps2_std: float
-    ps2_min: float
     ps2_max: float
     fs1_mean: float
     fs1_std: float
-    fs1_min: float
     fs1_max: float
 
 #from src.model import load_model
